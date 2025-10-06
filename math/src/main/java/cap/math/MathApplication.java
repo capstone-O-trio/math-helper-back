ff8b0d5be9d9e88f2ab398325048b3bd116099e3
gaeul3303 <rkdmfghd@naver.com>
2025-10-06 14:22:21 +0900

    feat:DB 연결 및 설계

--- math/src/main/java/cap/math/MathApplication.java
+++ math/src/main/java/cap/math/MathApplication.java
@@ -2,8 +2,10 @@ package cap.math;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MathApplication {
 
 	public static void main(String[] args) {
---
+++ math/src/main/java/cap/math/domain/Math.java
@@ -0,0 +1,34 @@
+package cap.math.domain;
+
+import cap.math.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Math extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String image;
+
+    private Integer answer;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "math", cascade = CascadeType.ALL)
+    private List<MathEntity> mathEntityList=new ArrayList<>();
+
+
+
+
+}
